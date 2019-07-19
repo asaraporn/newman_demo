93091ee5f8ddd8105d7c4995a69a470c33aff9ee
asaraporn <50699216+asaraporn@users.noreply.github.com>
2019-07-19 10:16:30 +0700

    Add files via upload
    Test

---
+++ mailNotification.py
@@ -0,0 +1,17 @@
+# https://myaccount.google.com/lesssecureapps
+
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login("asaraporn@addtechhub.com", "Hadsai.1")
+text = "Test by Python"
+
+server.sendmail("asaraporn@addtechhub.com", "hadsai.g@gmail.com", text)
+server.quit() # ปิดการเชื่อมต่อ
+
+
+
+
+
