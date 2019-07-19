49a5747904b7dea6880ab94a8c9438fd892b051f
asaraporn <50699216+asaraporn@users.noreply.github.com>
2019-07-19 10:20:14 +0700

    Add files via upload

--- mailNotification.py
+++ mailNotification.py
@@ -9,7 +9,7 @@ server.login("asaraporn@addtechhub.com", "Hadsai.1")
 text = "Test by Python"
 
 server.sendmail("asaraporn@addtechhub.com", "hadsai.g@gmail.com", text)
-server.quit() # ปิดการเชื่อมต่อ
+server.quit()
 
 
 
